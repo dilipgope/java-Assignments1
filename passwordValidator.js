fe58683805447d8caefd482f454988f7d14fbea1
dilipgope <dilipgopejio@gmail.com>
2023-03-04 22:04:10 +0530

    password validator

---
+++ passwordValidator.js
@@ -0,0 +1,3 @@
+let passwd = "abc"
+let inputPassword = "abc"
+let outputMsg = passwd == inputPassword ? console.log("Password Matched. Password validation successful.") : console.log("Password did't match. Password validation unsuccessful");
